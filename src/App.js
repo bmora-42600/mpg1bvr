ac8a16ddaabb94ede7cfa7b6c358243682aa4664
Briam <doitxr@gmail.com>
2022-09-11 15:50:55 -0600

    Using Aframe

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+require('aframe-stereo-component');
+require('aframe');
+
+function App() {
+  return (
+    <div className="App">
+      <a-scene antialias="true">
+        <a-assets>
+          <img id="right" src="img/stereo1r.jpg"/>
+          <img id="left" src="img/stereo1l.jpg"/>
+        </a-assets>
+        <a-entity camera look-controls position="0 0 0" stereocam="eye:left;"></a-entity>
+
+        <a-sky id="sky1" src="#left" stereo="eye:left"></a-sky>
+        <a-sky id="sky2" src="#right" stereo="eye:right"></a-sky>
+      </a-scene>
+    </div>
+  );
+}
+
+export default App;
