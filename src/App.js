25b57c2de7073d31e6fe59d14686d1cab8cf5609
Briam <briam_mora@gensler.com>
2022-09-14 13:40:43 -0600

    sounds

--- src/App.js
+++ src/App.js
@@ -10,46 +10,65 @@ require('aframe');
 
 function App() {
 
+  const [actualLocation, setActualLocation] = useState(Tour.locations[0].id);
+
   useEffect(()=>{
+    window.localStorage.setItem("actualLocation",Tour.locations[0].id)
     document.querySelector('a-scene').addEventListener('loaded', function () {
       setTimeout(()=>document.getElementById("loading-page").style.visibility = "hidden",3000);
     })
     window.addEventListener("go-to", function(e) {
-      var fadeEl = document.querySelector("#fade")
-      fadeEl.emit("fadein");
-      setTimeout(()=>{
-        var skybox = document.querySelector("#sky")
-        var goToWaypoints = document.querySelector(`#${e.detail.to}`)
-        Tour.locations.forEach(location => {
-          let waypoints = document.getElementById(location.id);
-          waypoints.setAttribute("visible",false)
-          waypoints.setAttribute("scale","0 0 0")
-        })
-        goToWaypoints.setAttribute("visible",true)
-        goToWaypoints.setAttribute("scale","1 1 1")
-        skybox.setAttribute("src", `#img-${e.detail.to}`);
-        fadeEl.emit("fadeout")
-      },1000)
+      GoTo(e.detail.to);
   });
   },[])
 
-  const GoTo = to => {
+  const GoTo = (to) => {
+    let actualLocation = window.localStorage.getItem("actualLocation");
+    var fadeEl = document.querySelector("#fade")
+    fadeEl.emit("fadein");
+    let stepsSound = document.getElementById("steps-sound");
+    stepsSound.components.sound.playSound();
+    setTimeout(()=>{
+      var skybox = document.querySelector("#sky")
+      var actualWaypoints = document.querySelector(`#${Tour.locations.filter(location => location.id === actualLocation)[0].id}`)
+      var goToWaypoints = document.querySelector(`#${to}`)
+      actualWaypoints.setAttribute("visible",false)
+      actualWaypoints.childNodes.forEach(child => {
+        child.classList.remove("clickable");
+      })
+      goToWaypoints.setAttribute("visible",true)
+      goToWaypoints.childNodes.forEach(child => {
+        child.classList.add("clickable");
+      })
+      skybox.setAttribute("src", `#img-${to}`);
+      fadeEl.emit("fadeout")
+    },1000)
+    setActualLocation(to);
+    window.localStorage.setItem("actualLocation",to)
+  }
+
+  const DispatchGoToEvent = to => {
     let event = new CustomEvent("go-to", {'detail': {
       to: to,
     }});
     window.dispatchEvent(event);
   }
+
   return (
     <div className="App">
       <div id="loading-page">
         <img className="logo" src={"img/logos.png"} alt="logos" />
       </div>
       <div id="web-ui">
-        <img className="location-btn" onClick={() => GoTo("pano1")} src={"img/ButtonONE.png"} alt="button one" />
-        <img className="location-btn" onClick={() => GoTo("pano2")} src={"img/ButtonONW.png"} alt="button onw" />
-        <img className="location-btn" onClick={() => GoTo("pano3")} src={"img/ButtonPNW.png"} alt="button pnw" />
-        <img className="location-btn" onClick={() => GoTo("pano4")} src={"img/ButtonPSW.png"} alt="button psw" />
-        <img className="location-btn" onClick={() => GoTo("pano5")} src={"img/ButtonPCINT.png"} alt="button pcint" />
+      {Tour.locations.map((location) => {
+        return <img className={`location-btn ${actualLocation===location.id?"selected":""}`}
+          onClick={() => DispatchGoToEvent(location.id)} 
+          src={location.thumbnail} 
+          alt={`button ${location.id}`} />
+      }
+        
+        )
+      }
       </div>
       <div id="web-logos">
         <img className="logo" src={"img/logos.png"} alt="logos" />
@@ -71,12 +90,12 @@ function App() {
         </a-camera>
 
         {Tour.locations.map((location,index) => {
-          return <a-entity id={location.id} visible={index===0?"true":"false"} scale={index===0?"1 1 1":"0 0 0"}>
+          return <a-entity id={location.id} visible={index===0?"true":"false"}>
             {location.waypoints.map( waypoint =>
               <a-image 
                 position={`${waypoint.position[0]} ${waypoint.position[1]} ${waypoint.position[2]}`}
                 scale="2 2.5 1"
-                class="clickable" 
+                class={index===0?"clickable":""} 
                 waypoint={`to:${waypoint.to}`} 
                 src="#waypoint"></a-image>
             )}
@@ -85,6 +104,9 @@ function App() {
         
         <a-entity laser-controls="hand: right" raycaster="objects: .clickable; lineColor: red; lineOpacity: 0.5;"></a-entity>
         <a-sky id="sky" src={`#img-${Tour.locations[0].id}`}></a-sky>
+
+        <a-sound id="ambient-sound" src="src: url(sound/ambient.mp3)" loop="true" autoplay="true"></a-sound>
+        <a-sound id="steps-sound" src="src: url(sound/steps.mp3)"></a-sound>
         
       </a-scene>
     </div>
--- src/components/waypoint/waypoint.js
+++ src/components/waypoint/waypoint.js
@@ -14,5 +14,18 @@ AFRAME.registerComponent("waypoint", {
         }});
         window.dispatchEvent(event);
       })
+
+      this.el.addEventListener("click", e => {
+        let event = new CustomEvent("go-to", {'detail': {
+          to: this.data.to,
+        }});
+        window.dispatchEvent(event);
+      })
+      this.el.addEventListener('mouseenter', (e) => {
+        this.el.setAttribute('scale', '2.5 3 1');
+      })
+      this.el.addEventListener('mouseleave', (e) => {
+        this.el.setAttribute('scale', '2 2.5 1');
+      })
     }
 })
