2696843d15404b836bf456d19983cb9dff5c8e82
Briam <briam_mora@gensler.com>
2022-09-15 13:08:18 -0600

    Update map.js

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -9,7 +9,9 @@ AFRAME.registerComponent("map", {
         let position = this.el.getAttribute("position");
         var direction = new window.THREE.Vector3();
         this.el.sceneEl.camera.getWorldDirection(direction);
+        direction.y = 0;
+        direction.multiplyScalar(3);
         direction.y = this.el.sceneEl.camera.position.y;
-        position.lerp(direction.multiplyScalar(3),0.01);
+        position.lerp(direction,0.01);
     }
 })
